430a41ade9b3bbafc821b62f2bcea9228929bd39
THbrandV <mirko.schoerner@th-brandenburg.de>
2019-05-30 14:57:23 +0200

    Initial commit

--- www/js/Main.js
+++ www/js/Main.js
@@ -36,7 +36,7 @@ import * as entities from "./model/MyEntities.js";
 /* application libraries: view controllers */
 import MyInitialViewController from "./controller/MyInitialViewController.js";
 // TODO-REPEATED: import any further view controllers here
-
+import ListviewViewController from "./controller/ListviewViewController.js";
 
 // we export the framework modules required by the application and the application modules required by the framework
 export {
@@ -48,6 +48,7 @@ export {
     GenericCRUDImplRemote,
     GenericDialogTemplateViewController,
     /* TODO: export the mapHolder in case it is used by the application */
+    ListviewViewController //noch rein? -> ,
     // mapHolder,
     /* application modules */
     MyApplication,
